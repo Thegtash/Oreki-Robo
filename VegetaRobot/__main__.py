0611cb2e50a5ae3e28ea3fb035430b16adbbd575
« ͸ Tʜᴇ ɢᴛᴀsʜ ۩ <114175128+Thegtash@users.noreply.github.com>
2022-10-08 10:16:43 +0530

    Update __main__.py

--- VegetaRobot/__main__.py
+++ VegetaRobot/__main__.py
@@ -81,9 +81,9 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-PM_START_TEXT = """ ┗►  *What's Up Yoo! * ◄┛
+PM_START_TEXT = """ ┗►  *What's Up Broo! * ◄┛
 
-~~ *I'm made with Saiyans blood* ~~
+~~ *I'm a Powerful Robot* ~~
 
 ~ *Wanna see my commands*[❗]({})
 *below click the help button!* ~
@@ -94,16 +94,16 @@ PM_START_TEXT = """ ┗►  *What's Up Yoo! * ◄┛
 buttons = [
     [
         InlineKeyboardButton(
-                            text="☑ Add Vegeta To Groups ☑",
+                            text="☑ Add Oreki To Groups ☑",
                             url=f"t.me/{BOT_USERNAME}?startgroup=true"),
                     ],
                      [
-                       InlineKeyboardButton(text="SUPPORT", url=f"https://t.me/{SUPPORT_CHAT}"),
-                       InlineKeyboardButton(text="UPDATES",  url=f"https://t.me/{UPDATES_CHANNEL}"),
+                       InlineKeyboardButton(text="SUPPORT", url=f"https://t.me/TEAM_WOLF_004"),
+                       InlineKeyboardButton(text="UPDATES",  url=f"https://t.me/TEAMWOLF_UPDATES"),
                     ],
                    [
-                       InlineKeyboardButton(text="NETWORK", url=f"https://t.me/nandhabots"),
-                       InlineKeyboardButton(text="LOGS", url=f"https://t.me/vegetalogs"),
+                       InlineKeyboardButton(text="NETWORK", url=f"https://t.me/wolfnetwrk"),
+                       InlineKeyboardButton(text="LOGS", url=f"https://t.me/wolf_logs"),
                 ],[ InlineKeyboardButton(text="COMMANDS HELP", callback_data="help_back"
          ),
     ],
@@ -116,14 +116,14 @@ HELP_STRINGS = """ *Hello There! Here you can get all of the help commands!
 Below Click the module you know about module commands!*
 """
 
-HELP_MSG = "Click the button below to get help manu in your pm."
-DONATE_STRING = """*don't need donate I'm free for everyone add your group's in @VegetaRobot this is my donate🙂*"""
-HELP_IMG= "https://telegra.ph/file/9d2c6e3b28afe7619856e.jpg"
-GROUPSTART_IMG= "https://telegra.ph/file/1cbafa58dda18528f9e0c.mp4"
+HELP_MSG = "Click the button below to get help menu in your pm."
+DONATE_STRING = """*don't need donate I'm free for everyone add your group's in @OrekiXPro_Bot this is my donate🙂*"""
+HELP_IMG= "https://te.legra.ph/file/0398eed3f859ee56d9f9a.jpg"
+GROUPSTART_IMG= "https://te.legra.ph/file/6ff50ae4c364893982136.mp4"
 
-VEGETA_IMG = ( "https://telegra.ph//file/a47f16c936dbbd4165399.jpg",
-               "https://telegra.ph//file/5026650d5e3f0b83c6d29.jpg",
-               "https://telegra.ph/file/561fa547f3c4940c95ddf.jpg",)       
+VEGETA_IMG = ( "https://te.legra.ph/file/5ff7507ea728df52ecc39.jpg",
+               "https://te.legra.ph/file/6ee4d3b521fc5d2f6507f.jpg",
+               "https://te.legra.ph/file/d5a6f597b07e5ffcc58d0.jpg       
 
 IMPORTED = {}
 MIGRATEABLE = []
