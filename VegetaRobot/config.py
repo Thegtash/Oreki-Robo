cab9d3a33cf14cdf69f01d90f039eba4ad2e82da
« ͸ Tʜᴇ ɢᴛᴀsʜ ۩ <114175128+Thegtash@users.noreply.github.com>
2022-10-08 09:16:29 +0530

    Update config.py

--- VegetaRobot/config.py
+++ VegetaRobot/config.py
@@ -17,24 +17,24 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 12345   # integer value, dont use ""
-    API_HASH = "awew"
-    TOKEN = "wow"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1491497760  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "nandhaxd"
-    SUPPORT_CHAT = 'vegetasupport'  #Your own group for support, do not add the @
-    UPDATES_CHANNEL = 'vegetaUpdates' #Your own channel for Updates of bot, Do not add @
-    JOIN_LOGGER = -1001543354286  #Prints any new group the bot is added to, prints just the name and ID.
+    API_ID = 7623447   # integer value, dont use ""
+    API_HASH = "e02d7f441208afa97fccf72517c14a10"
+    TOKEN = "5608162397:AAEiImofcExh6Du7WmchgpoqPGRzAPFHj4c"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 51897675  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "awesome_gtashxd"
+    SUPPORT_CHAT = 'team_wolf_004'  #Your own group for support, do not add the @
+    UPDATES_CHANNEL = 'teamwolf_updates' #Your own channel for Updates of bot, Do not add @
+    JOIN_LOGGER = - -1767355847  #Prints any new group the bot is added to, prints just the name and ID.
     REM_BG_API_KEY = "dxsh728mZMDmj4ijSZCNPZig"
-    EVENT_LOGS = -1001543354286  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    EVENT_LOGS = -1806602433  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     LOAD = []
     NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
     WEBHOOK = None
     INFOPIC = True
     URL = None
     SPAMWATCH_API = ""  # go to support.spamwat.ch to get key -
-    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
-    BOT_ID = "2128359921"
+    SPAMWATCH_SUPPORT_CHAT = "@Team_Wolf_004"
+    BOT_ID = "5608162397"
     
     DRAGONS = get_user_list('elevated_users.json', 'sudos')
 
